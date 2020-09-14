67810c5172c2dd64a145329f450a7e0f31bfd52b
tefi-anbessa <frankrobinson99@hotmail.com>
2020-09-14 11:22:19 +0700

    Add selection of main image from thumbnails

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,13 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo > img")
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function () {
+      // Set Clicked thumbnail as main image
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+    });
+  });
 }
