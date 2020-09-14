7d44a780d6b02aab0ed20765f0c8109e7be03612
tefi-anbessa <frankrobinson99@hotmail.com>
2020-09-14 12:42:40 +0700

    Add update information section on selection, preload images

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,31 @@
 function activateGallery() {
 
-  let thumbnails = document.querySelector("#gallery-thumbs").
-                            querySelectorAll("img");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo > img")
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title       = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
+
+
   thumbnails.forEach(function(thumbnail) {
+    // Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+    
+    // put click event watchers on the thumbnails
     thumbnail.addEventListener("click", function () {
+
       // Set Clicked thumbnail as main image
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Set title and description to match
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
